07e5220b4ba062deb1bc88e6711b81d886a08fb4
lelong1304 <lelong1304@gmail.com>
2022-11-06 11:47:02 +0100

    fix dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { CurrencyComponent } from './home/currency.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
@@ -16,20 +15,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import {ConfigService} from './config/service';
-import { TransactionExchangeComponent } from './transaction-exchange/transaction-exchange.component';
-import { TransactionListComponent } from './transaction-list/transaction-list.component';
 import { MainNavbarComponent } from './main-navbar/main-navbar.component';
-import { ExchangeRateAllComponent } from './exchange-rate-all/exchange-rate-all.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    CurrencyComponent,
-    TransactionExchangeComponent,
-    TransactionListComponent,
-    MainNavbarComponent,
-    ExchangeRateAllComponent
+    MainNavbarComponent
   ],
   imports: [
     BrowserModule,
